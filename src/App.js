c05d7bb545fc16abaeba3195f969ffb9dfcc489f
Noah Greff <greffnoah@gmail.com>
2022-12-05 13:08:02 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import { useState } from 'react'
+
+const { Configuration, OpenAIApi } = require('openai')
+const configuration = new Configuration({
+	// apiKey: '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'
+})
+const openai = new OpenAIApi(configuration)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [image, setImage] = useState()
+	const { transcript, listening, resetTranscript } = useSpeechRecognition()
+
+	const generateImage = async () => {
+		const response = await openai.createImage({
+			prompt: transcript,
+			n: 1,
+			size: '256x256'
+		})
+		const url = response.data.data[0].url
+		console.log(url)
+		setImage(url)
+	}
+
+	return (
+		<div className="App">
+			<div>
+				<p>Microphone: {listening ? 'on' : 'off'}</p>
+				<button onClick={SpeechRecognition.startListening}>Start</button>
+				<button onClick={SpeechRecognition.stopListening}>Stop</button>
+				<button onClick={resetTranscript}>Reset</button>
+				<p>{transcript}</p>
+			</div>
+			<button onClick={generateImage}>Generate</button>
+			{image ? <img src={image} /> : null}
+		</div>
+	)
 }
 
-export default App;
+export default App
