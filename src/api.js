d3606030bef516b05d6fc6a3ab43631b55abdbaa
Noah Greff <greffnoah@gmail.com>
2023-01-12 11:03:18 +0100

    using react env variables

--- src/api.js
+++ src/api.js
@@ -1,19 +1,16 @@
 import axios from 'axios'
 
-const OPENAI_API_KEY = ''
-const THESAURUS_API_KEY = ''
-
 export const generateImage = async prompt => {
 	const options = { prompt, size: '512x512', n: 1 }
 	const headers = {
-		Authorization: 'Bearer ' + OPENAI_API_KEY
+		Authorization: 'Bearer ' + process.env.REACT_APP_OPENAI_KEY
 	}
 	const response = await axios.post('https://api.openai.com/v1/images/generations', options, { headers })
 	return response.data.data[0].url
 }
 
 export const getSynonymAndAntonym = async word => {
-	const headers = { 'X-Api-Key': THESAURUS_API_KEY }
+	const headers = { 'X-Api-Key': process.env.REACT_APP_NINJA_KEY }
 	const res = await axios.get('https://api.api-ninjas.com/v1/thesaurus?word=' + word, { headers })
 	const { synonyms, antonyms } = res.data
 
