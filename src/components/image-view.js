6beef78a86c7ab520c7d8bcd2f463d530cbb1638
Noah Greff <greffnoah@gmail.com>
2023-01-16 12:52:43 +0100

    updated ui when image errors out

--- src/components/image-view.js
+++ src/components/image-view.js
@@ -3,34 +3,19 @@ import { generateImage } from '../api'
 
 const ImageView = ({ prompt }) => {
 	const [image, setImage] = useState()
-	const [error, setError] = useState()
 
 	useEffect(() => {
 		if (prompt) {
-			generateImage(prompt).then(setImage).catch(setError)
+			generateImage(prompt)
+				.then(setImage)
+				.catch(() => generateImage('error stop sign').then(setImage))
 		} else {
 			setImage(null)
-			setError(null)
 		}
 	}, [prompt])
 
-	if (error) {
-		return (
-			<div className="image-view" style={{ color: 'red' }}>
-				{/* {error.message ?? error} */}
-				Prompt provided contained unsafe words
-			</div>
-		)
-	}
 	if (prompt) {
-		if (image) {
-			return (
-				<div className="image-view">
-					<img src={image} />
-				</div>
-			)
-		}
-		return <div className="image-view inputs">Processing...</div>
+		return <div className="image-view inputs">{image ? <img src={image} /> : 'Processing...'}</div>
 	}
 	return <div className="image-view inputs">No prompt</div>
 }
